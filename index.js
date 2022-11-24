d0ffb8ccf9cfbf966e87f98e6b81ecc26d03d161
NahidulNoman <nahidulna5@gmail.com>
2022-11-24 15:17:26 +0600

    category items added home page

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // middle wear
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('you hit the road server is running..')
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a31ucvz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const categoryCollection = client.db("nextCarSell").collection("category");
+    const categoryDetailsCollection = client.db("nextCarSell").collection("categoryDetails");
+
+    // get home page category 
+    app.get("/category", async (req, res) => {
+      const query = {};
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    
+
+
+  } finally {
+
+  }
+}
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("you hit the road server is running..");
 });
 
-app.listen(port , () => {
-    console.log(`your hit the road server is running port ${port}`)
+app.listen(port, () => {
+  console.log(`your hit the road server is running port ${port}`);
 });
